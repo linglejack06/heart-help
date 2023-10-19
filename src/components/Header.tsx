828f2b2167a18af8d0a888419b5b43ec2b9a81af
Jack Lingle <captainjack0818@gmail.com>
2023-10-19 18:57:00 -0400

    update size of header text

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 const Header = () => (
   <div className="text-white bg-red-700 p-2 corner rounded-b-md shadow-md sticky top-0">
-    <h1 className="ml-5 text-lg font-bold">Heart Help</h1>
+    <h1 className="ml-5 text-2xl font-extrabold">Heart Help</h1>
   </div>
 );
 
