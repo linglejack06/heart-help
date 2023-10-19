f28d53235892668f47a5f37e96e01314d01df158
Jack Lingle <captainjack0818@gmail.com>
2023-10-19 17:50:09 -0400

    change link to button for better clickability

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,10 +1,15 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function Welcome() {
+  const navigate = useNavigate();
+  const onClick = () => navigate("/pain-calculation");
   return (
-    <div className="mx-auto bg-red-100 text-red-700 font-bold p-4 rounded-md hover:text-white hover:bg-red-700 hover:shadow-lg hover:cursor-pointer hover:p-6 transition-all hover:rounded-xl border-2 border-red-700">
-      <Link to="/pain-calculation">Am I having a heart attack</Link>
-    </div>
+    <button
+      onClick={onClick}
+      className="mx-auto bg-red-100 text-red-700 font-bold p-4 rounded-md hover:text-white hover:bg-red-700 hover:shadow-lg hover:cursor-pointer hover:p-6 transition-all hover:rounded-xl border-2 border-red-700"
+    >
+      Am I Having A Heart Attack?
+    </button>
   );
 }
 
